4f2fad35e1f00c85a7c410d0fb56c4bbf5f5f19f
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-10-29 00:34:34 +0100

    Add subscription and tenant retrieval

--- authorizer.go
+++ authorizer.go
@@ -16,11 +16,27 @@ func main() {
 		log.Fatal(err)
 	}
 	subscriptionsClient.Authorizer = authorizer
-	results, err := subscriptionsClient.List(context.TODO())
+	subscriptionsPage, err := subscriptionsClient.List(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, sub := range results.Values() {
-		log.Printf("Subscription name: %s and ID: %s", *sub.DisplayName, *sub.ID)
+	//var sId string
+	for _, sub := range subscriptionsPage.Values() {
+		log.Printf("Subscription name: %s and ID: %s\n", *sub.DisplayName, *sub.SubscriptionID)
+		//sId = *sub.SubscriptionID
+	}
+	tenantsClient := subscriptions.NewTenantsClient()
+	tenantsClient.Authorizer = authorizer
+	tenantsPage, err := tenantsClient.List(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	//var tId string
+	for _, ten := range tenantsPage.Values() {
+		log.Printf("Some id: %s and some other id: %s\n", *ten.ID, *ten.TenantID)
+		//tId = *ten.TenantID
+	}
+	if len(subscriptionsPage.Values()) != 1 || len(tenantsPage.Values()) != 1 {
+		log.Fatal("There is more than 1 subscription or tenant")
 	}
 }
