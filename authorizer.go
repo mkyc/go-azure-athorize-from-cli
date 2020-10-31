31c978f94397d326a56fd2f3a736c8e458a83683
Mateusz Kyc <mateusz.kyc@gmail.com>
2020-10-31 19:36:25 +0100

    Add service principal creation flow

--- authorizer.go
+++ authorizer.go
@@ -3,12 +3,32 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/Azure/azure-sdk-for-go/profiles/2019-03-01/resources/mgmt/subscriptions"
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/authorization/mgmt/authorization"
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/graphrbac/graphrbac"
 	"github.com/Azure/go-autorest/autorest/azure"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/Azure/go-autorest/autorest/date"
+	"github.com/Azure/go-autorest/autorest/to"
+	uuid "github.com/satori/go.uuid"
+	"github.com/sethvargo/go-password/password"
 )
+
+const (
+	cloudName        = "AzurePublicCloud"
+	defaultPublisher = "Microsoft Services"
+	roleName         = "Contributor"
+	appName          = "20201031-auto-test-1"
+)
+
+type Credentials struct {
+	appId        string
+	password     string
+	tenant       string
+	subscription string
+}
 
 func main() {
 
@@ -22,11 +42,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var subscriptionId string
 	subscriptionsCount := 0
 	for subscriptionsIterator.NotDone() {
 		subscriptionsCount++
 		sub := subscriptionsIterator.Value()
 		log.Printf("Subscription name: %s and ID: %s\n", *sub.DisplayName, *sub.ID)
+		subscriptionId = *sub.SubscriptionID
 		err = subscriptionsIterator.NextWithContext(context.TODO())
 		if err != nil {
 			log.Fatal(err)
@@ -39,13 +61,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	var tId string
+	var tenantId string
 	tenantsCount := 0
 	for tenantsIterator.NotDone() {
 		tenantsCount++
 		ten := tenantsIterator.Value()
-		log.Printf("Some tenant id: %s and some other id: %s\n", *ten.ID, *ten.TenantID)
-		tId = *ten.TenantID
+		log.Printf("Some tenant id: %s\n", *ten.ID)
+		tenantId = *ten.TenantID
 		err = tenantsIterator.NextWithContext(context.TODO())
 		if err != nil {
 			log.Fatal(err)
@@ -56,14 +78,14 @@ func main() {
 		log.Fatal("There is more than 1 subscription or tenant")
 	}
 
-	env, err := azure.EnvironmentFromName("AzurePublicCloud")
+	env, err := azure.EnvironmentFromName(cloudName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	graphAuthorizer, err := auth.NewAuthorizerFromCLIWithResource(env.GraphEndpoint)
 
-	spClient := graphrbac.NewServicePrincipalsClient(tId)
+	spClient := graphrbac.NewServicePrincipalsClient(tenantId)
 	spClient.Authorizer = graphAuthorizer
 
 	spIterator, err := spClient.ListComplete(context.TODO(), "")
@@ -72,8 +94,8 @@ func main() {
 	}
 	for spIterator.NotDone() {
 		sp := spIterator.Value()
-		if *sp.PublisherName != "Microsoft Services" {
-			log.Printf("Service Principal: %s\n", *sp.DisplayName)
+		if *sp.PublisherName != defaultPublisher {
+			log.Printf("Service Principal Name: %s, ID: %s\n", *sp.DisplayName, *sp.ObjectID)
 		}
 		err = spIterator.NextWithContext(context.TODO())
 		if err != nil {
@@ -81,7 +103,7 @@ func main() {
 		}
 	}
 
-	appClient := graphrbac.NewApplicationsClient(tId)
+	appClient := graphrbac.NewApplicationsClient(tenantId)
 	appClient.Authorizer = graphAuthorizer
 
 	appIterator, err := appClient.ListComplete(context.TODO(), "")
@@ -96,4 +118,103 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+
+	roleDefinitionClient := authorization.NewRoleDefinitionsClient(subscriptionId)
+	roleDefinitionClient.Authorizer = resourceManagerAuthorizer
+
+	roleDefinitionIterator, err := roleDefinitionClient.ListComplete(context.TODO(), "/subscriptions/"+subscriptionId, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var roleId string
+	for roleDefinitionIterator.NotDone() {
+		rd := roleDefinitionIterator.Value()
+		if *rd.RoleName == roleName {
+			roleId = *rd.ID
+			log.Printf("RoleDefinition: %s\n", *rd.RoleName)
+		}
+		err = roleDefinitionIterator.NextWithContext(context.TODO())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	keyId := uuid.NewV4()
+	pass, err := password.Generate(32, 10, 0, false, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := &date.Time{
+		Time: time.Now(),
+	}
+	t2 := &date.Time{
+		Time: t.AddDate(2, 0, 0),
+	}
+	app, err := appClient.Create(context.TODO(), graphrbac.ApplicationCreateParameters{
+		DisplayName:             to.StringPtr(appName),
+		IdentifierUris:          &[]string{"https://" + appName},
+		AvailableToOtherTenants: to.BoolPtr(false),
+		Homepage:                to.StringPtr("https://" + appName),
+		PasswordCredentials: &[]graphrbac.PasswordCredential{{
+			StartDate:           t,
+			EndDate:             t2,
+			KeyID:               to.StringPtr(keyId.String()),
+			Value:               to.StringPtr(pass),
+			CustomKeyIdentifier: to.ByteSlicePtr([]byte(appName)),
+		}},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	appJson, err := app.MarshalJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("\n===========\nAPPLICATION\n%v\n===========\n", string(appJson))
+
+	sp, err := spClient.Create(context.TODO(), graphrbac.ServicePrincipalCreateParameters{
+		AppID:          app.AppID,
+		AccountEnabled: to.BoolPtr(true),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	spJson, err := sp.MarshalJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("\n===========\nSERVICE PRINCIPAL\n%v\n===========\n", string(spJson))
+
+	roleAssignmentClient := authorization.NewRoleAssignmentsClient(subscriptionId)
+	roleAssignmentClient.Authorizer = resourceManagerAuthorizer
+
+	roleAssignmentName := uuid.NewV4()
+	for i := 0; i < 30; i++ {
+		ra, err := roleAssignmentClient.Create(context.TODO(), "/subscriptions/"+subscriptionId, roleAssignmentName.String(), authorization.RoleAssignmentCreateParameters{
+			Properties: &authorization.RoleAssignmentProperties{
+				RoleDefinitionID: to.StringPtr(roleId),
+				PrincipalID:      sp.ObjectID,
+			},
+		})
+		if err != nil {
+			log.Println(err)
+			time.Sleep(1 * time.Second)
+			continue
+		} else {
+			raJson, err := ra.MarshalJSON()
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("\n===========\nROLE ASSIGNMENT\n%v\n===========\n", string(raJson))
+			break
+		}
+	}
+
+	c := &Credentials{
+		appId:        *sp.AppID,
+		password:     pass,
+		tenant:       tenantId,
+		subscription: subscriptionId,
+	}
+	log.Printf("\n===========\nCREDENCIALS\n%+v\n===========\n", c)
 }
--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ require (
 	github.com/Azure/azure-sdk-for-go v48.0.0+incompatible
 	github.com/Azure/go-autorest/autorest v0.11.10
 	github.com/Azure/go-autorest/autorest/azure/auth v0.5.3
-	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0
+	github.com/Azure/go-autorest/autorest/to v0.4.0
 	github.com/Azure/go-autorest/autorest/validation v0.3.0 // indirect
 	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/sethvargo/go-password v0.2.0
 	github.com/shopspring/decimal v1.2.0 // indirect
 	github.com/spf13/cobra v1.1.1 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
